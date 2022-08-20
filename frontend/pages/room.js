d144b10d0c69f257494ca846227d773dc9af6586
jyp930 <jyp930@gmail.com>
2022-08-20 15:07:23 +0900

    qrcode 삽입

--- frontend/pages/room.js
+++ frontend/pages/room.js
@@ -1,13 +1,21 @@
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import QRCode from 'qrcode';
+import { useEffect } from 'react';
 
 export default function Room({ sessionId }) {
+  useEffect(() => {
+    const canvas = document.getElementById('roomCode');
+    QRCode.toCanvas(canvas, location.href, { color: { dark: '#226AF6' } });
+  }, []);
+
   return (
     <>
       <NextSeo title="Room" description="TODO" />
       <h2>Room</h2>
       <p>person 1: jyp</p>
       <p>person 2: su</p>
+      <canvas id="roomCode"></canvas>
       <Link href={{ pathname: '/game', query: { session: sessionId } }}>
         <a>Start!</a>
       </Link>
